0a0dfa1f2e05d5fc2ec3602c450088bd6dea038e
Benjamin Balazs <benjaminbalazs@gmail.com>
2016-04-09 22:59:14 +0400

    tagline

--- addon/models/model.js
+++ addon/models/model.js
@@ -9,5 +9,7 @@ export default DS.Model.extend({
 	video_webm : DS.attr('string'),
 	cta_url : DS.attr('string'),
 	logo_svg : DS.attr('string'),
+	tagline_svg : DS.attr('string'),
+	hasTagline : DS.attr('boolean', { defaultValue: false } ),
 
 });
