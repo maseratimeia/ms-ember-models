e433a6051f6727e30ac2c38402b838b3affae4bd
Jovan Bilbija <jovan@tamra-c2.com>
2017-01-29 16:54:10 +0400

    Adding ar.

--- addon/models/model.js
+++ addon/models/model.js
@@ -5,6 +5,7 @@ export default DS.Model.extend({
 	name: DS.attr('string'),
 	video: DS.attr('string'),
 	photo_jpg : DS.attr('string'),
+	photo_jpg_ar : DS.attr('string'),
 	video_mp4 : DS.attr('string'),
 	video_webm : DS.attr('string'),
 	cta_url : DS.attr('string'),
